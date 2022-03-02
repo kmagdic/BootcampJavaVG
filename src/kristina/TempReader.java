f63929797f7d330f708d510fdda67ba41ac95b2d
krist <vujnovic.kristina1@gmail.com>
2022-03-02 16:27:36 +0100

    Temperatura

---
+++ src/kristina/TempReader.java
@@ -0,0 +1,21 @@
+package kristina;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class TempReader {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner s = new Scanner(new File("doc/temperature.txt"));
+        s.nextLine();
+        s.useDelimiter(", |\n");
+
+        while (s.hasNext()) {
+            String city = s.next();
+            String temp = s.next();
+
+            System.out.println("Ime grada" + city + "temp" + temp);
+        }
+
+    }
+}
