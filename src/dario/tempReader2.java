8a04e2699273a0b03bc93972eccdabc4d86d9c14
Dario Jakovljević <jakovljevic.dario@gmail.com>
2022-03-02 16:27:03 +0100

    datoteke-teamsiki -crikva

--- src/dario/tempReader2.java
+++ src/dario/tempReader2.java
@@ -5,11 +5,21 @@ import java.util.Scanner;
 
 public class tempReader2 {
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner scanner = new Scanner(new FileReader("doc/dario_temp.txt"));
+        Scanner scanner = new Scanner(new FileReader("doc/temp_croatia_2022-03-02.txt"));
         scanner.nextLine();
         scanner.useDelimiter(", |\n");
+        float tempMin = 100;
+        String gradMin = "";
         while (scanner.hasNext()) {
-            System.out.println("Grad: " + scanner.next() + " Temperatura: " + scanner.next());
+            String grad = scanner.next();
+            String StrTemperatura = scanner.next();
+            float temperatura = Float.parseFloat(StrTemperatura);
+            System.out.println("Grad: " + grad + " Temperatura: " + temperatura);
+            if (temperatura <= tempMin) {
+                tempMin = temperatura;
+                gradMin = grad;
+            }
         }
+        System.out.println("Najniža temperatura je: " + tempMin + " u gradu: " + gradMin);
     }
 }
