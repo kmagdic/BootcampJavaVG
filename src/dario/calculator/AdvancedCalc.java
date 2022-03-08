53e960b3981cca9d7f04d0586a7f196ad47ce725
Dario Jakovljević <jakovljevic.dario@gmail.com>
2022-03-08 14:40:55 +0100

    advanced kalkulator objekti dodatak

--- src/dario/calculator/AdvancedCalc.java
+++ src/dario/calculator/AdvancedCalc.java
@@ -3,7 +3,7 @@ package dario.calculator;
 public class AdvancedCalc extends Calculator {
     @Override
     public String supportedOps() {
-        return super.supportedOps() + "**, min, max";
+        return super.supportedOps() + "**, min, max, hypot => sqrt(x2 +y2)";
     }
 
     @Override
@@ -14,6 +14,8 @@ public class AdvancedCalc extends Calculator {
             return Math.min(a, b);
         } else if (oper.equals("max")) {
             return Math.max(a, b);
+        } else if (oper.equals("hypot")) {
+            return Math.hypot(a, b);
         } else {
             return super.calculate();
         }
