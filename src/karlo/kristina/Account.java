6e972e5471a1740ee5f4bdfaa07cb3960d95d0c4
Karlo Magdic <karlo.magdic@gmail.com>
2022-03-23 12:28:29 +0100

    SBS Kristina Stage 2

--- src/karlo/kristina/Account.java
+++ src/karlo/kristina/Account.java
@@ -4,12 +4,12 @@ import java.util.*;
 
 public class Account {
 
-    static Scanner scanner = new Scanner(System.in);
-    static Random random = new Random();
+    Random random = new Random();
 
     private String cardNumber;
     private String pin;
-    private static List<String> cardList = new ArrayList<String>();
+
+
 
     public String getNewCardNumber() {
         return cardNumber;
@@ -19,7 +19,7 @@ public class Account {
         return pin;
     }
 
-    public static String createNewCardNumber() {
+    public String createNewCardNumber() {
         String bin = "400000";
         int lowerAccNr = 100000000;
         int upperAccNr = 999999999;
@@ -37,101 +37,16 @@ public class Account {
         }
         int lastDigit = (sum * 9) % 10;
 
-        String cardNumber = digits + String.valueOf(lastDigit);
+        cardNumber = digits + String.valueOf(lastDigit);
 
         return cardNumber;
     }
 
-    public static String createNewPin() {
+    public String crateNewPin() {
         int lower = 1000;
         int upper = 9999;
-        String pin = String.valueOf(random.nextInt(upper - lower + 1) + lower);
+        pin = String.valueOf(random.nextInt(upper - lower + 1) + lower);
 
         return pin;
     }
-
-    public static void addCardToList(String cardNumber, String pin){
-        cardList.add(cardNumber);
-        cardList.add(pin);
-    }
-
-    public static boolean checkPin(String cardNumberCheck, String pinNumberCheck) {
-       for (String number : cardList) {
-            if (number.equals(cardNumberCheck) && cardList.get(cardList.indexOf(number)+1).equals(pinNumberCheck)) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    public static void accountMenu() {
-
-        String accountMenuSelect = "";
-
-        while(!accountMenuSelect.equals("0") && !accountMenuSelect.equals("2")) {
-
-            System.out.println("1. Balance \n" +
-                    "2. Log out \n" +
-                    "0. Exit");
-
-            accountMenuSelect = scanner.next();
-
-            switch (accountMenuSelect) {
-
-                case "1":
-                    System.out.println("Balance: 0");
-                    break;
-
-                case "2":
-                    System.out.println("You have successfully logged out!");
-                    break;
-
-                case "0":
-                    System.out.println("Bye!");
-                    break;
-            }
-        }
-    }
-
-    public static void main(String[] args) {
-
-        String mainMenuSelect = " ";
-
-        while (!mainMenuSelect.equals("0")) {
-
-            System.out.println("1. Create an account \n" +
-                    "2. Log into account \n" +
-                    "0. Exit");
-
-            mainMenuSelect = scanner.next();
-
-            if (mainMenuSelect.equals("1")) {
-
-                System.out.println("Your card has been created");
-                String newCardNumber = createNewCardNumber();
-                System.out.println("Your card number:");
-                System.out.println(newCardNumber);
-                String newPin = createNewPin();
-                System.out.println("Your card PIN:");
-                addCardToList(newCardNumber, newPin);
-                System.out.println(newPin);
-
-            } else if (mainMenuSelect.equals("2")) {
-
-                System.out.println("Enter your card number:");
-                String cardNumberCheck = scanner.next();
-                System.out.println("Enter your PIN:");
-                String pinNumberCheck = scanner.next();
-
-                if(checkPin(cardNumberCheck, pinNumberCheck)) {
-                    System.out.println("You have successfully logged in!");
-                    accountMenu();
-                } else {
-                    System.out.println("Wrong card number or PIN!");
-                }
-            }
-        }
-    }
-
 }
