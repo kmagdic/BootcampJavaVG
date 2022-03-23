ac59feac1916ebb6a089544ccb01cbd1704a8eaf
Karlo Magdic <karlo.magdic@gmail.com>
2022-03-23 15:33:01 +0100

    SQLlite works with id

--- src/karlo/studentdb/StudentMain.java
+++ src/karlo/studentdb/StudentMain.java
@@ -7,17 +7,20 @@ public class StudentMain {
         Student s1 = new Student("Mario", "Manđžukić", 4);
         Student s2 = new Student("Dario", "Srna", 3);
 
-        System.out.println(s1);
-        System.out.println(s2);
 
 
         StudentDaoSqlite dao = new StudentDaoSqlite("doc/students_karlo.db");
         dao.saveStudent(s1);
         dao.saveStudent(s2);
 
+
         List<Student> students = dao.listAll();
         for(Student s : students)
             System.out.println(s);
+
+
+        dao.deleteStudent(s2);
+
 
     }
 }
