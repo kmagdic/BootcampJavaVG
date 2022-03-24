74f881b2fc01feef3861a300ee598eac2f71776e
Dario Jakovljević <jakovljevic.dario@gmail.com>
2022-03-24 22:44:01 +0100

    sum value from dataset file

---
+++ src/dario/datoteke/newLine/MainFiles.java
@@ -0,0 +1,22 @@
+package dario.datoteke.newLine;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class MainFiles {
+    public static void main(String[] args) {
+        File file = new File("doc/dataset_91033.txt");
+        long sum = 0L;
+
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNext()) {
+                long tmpNum = Integer.parseInt(scanner.nextLine());
+                sum += tmpNum;
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("No file found: doc/dataset_91033.txt");
+        }
+
+        System.out.println(sum);
+    }
+}
